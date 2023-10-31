144c0b8297d4b010bc3a02c57cf596620bec1a93
Chinmay <chinz.sandur07@gmail.com>
2023-10-31 14:00:38 +0530

    add Js example of let

--- 02/index.js
+++ 02/index.js
@@ -4,8 +4,14 @@ var name = "chinmay";
 var channel ;
 channel = ",Republic";
 console.log(name ,channel);
+var city = "delhi";
+const Name = "Chinmay";
+console.log(Name);
+// name = "hari";
+// console.log(name);
 
-const name = "Chinmay";
-console.log(name);
-name = "hari";
-console.log(name);
+
+{
+    let city = "rampur"
+    console.log(city);
+}
