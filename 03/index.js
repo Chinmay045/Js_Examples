0185a2f7cc73c32d94b21ceccd20f7f6eb4839ac
Chinmay <chinz.sandur07@gmail.com>
2023-11-01 13:47:49 +0530

    add let keyword

--- 03/index.js
+++ 03/index.js
@@ -1,3 +1,6 @@
-var name = "Chinmay";
+let name = "Chinmay";
 console.log("My string is " + name);
 console.log("Data type is " + (typeof name));
+
+let marks = 34;
+console.log("Data type is " + (typeof marks));
