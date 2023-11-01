4572de2a63184274f4966a16f523ed7dafaedbab
Chinmay <chinz.sandur07@gmail.com>
2023-11-01 15:38:14 +0530

    add example of indexOf and other methods

--- 05/index.js
+++ 05/index.js
@@ -2,3 +2,16 @@ console.log("we are at Chapter 5");
 const name = "Chimnay";
 const greeting = "Good morning";
 console.log(greeting + " " + name);
+
+let html;
+
+html = "<h1> This is heading</h1>" +
+"<p> This is my Para</p>";
+
+html = html.concat('this',' str2');
+console.log(html.length);
+console.log(html.toLocaleLowerCase());
+console.log(html.toLocaleUpperCase());
+console.log(html);
+
+console.log(html.indexOf('this'));
