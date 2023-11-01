aef25edf622d44134b394f6b1bdae9d97505362e
Chinmay <chinz.sandur07@gmail.com>
2023-11-01 14:06:42 +0530

    add example on type conversion of string

--- 04/index.js
+++ 04/index.js
@@ -1,7 +1,10 @@
 console.log("Welcome to tut5");
 let myVar;
 myVar = String(34);
-console.log(myVar ,(typeof myVar));
+console.log(myVar, (typeof myVar));
 
 let bool = String(true);
-console.log(bool, (typeof myVar));
+console.log(bool, (typeof bool));
+
+let arr = String([1,2,3,4,5]);
+console.log(arr.length ,(typeof arr));
