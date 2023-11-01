f0914daa637d9cbadef4ce8de46f906ae5fe0d08
Chinmay <chinz.sandur07@gmail.com>
2023-11-01 13:53:25 +0530

    add object example

--- 03/index.js
+++ 03/index.js
@@ -7,3 +7,14 @@ console.log("Data type is " + (typeof marks));
 
 let isDriver = true;
 console.log("Data type is " +(typeof isDriver));
+
+myArr = [1,2,3,4,45,6,false ,"string"];
+console.log("data type is " +(typeof myArr));
+
+let SMarks ={
+    harry :89,
+    Shubham:36,
+    "Rohan Das":34
+}
+
+console.log(SMarks);
