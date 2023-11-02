5cd0c321dec9cbb943c5f881c85ce34f942f087a
Chinmay <chinz.sandur07@gmail.com>
2023-11-02 10:50:12 +0530

    add example  of while loop

--- 07/index.js
+++ 07/index.js
@@ -2,22 +2,28 @@ console.log("hello");
 
 let age = 19;
 
-if(age == 19){
+if (age == 19) {
     console.log("It is true");
 }
-else{
+else {
     console.log("It is false");
 }
 
 let doesdrive = true;
 // let age =18
-if(doesdrive &&  age >18){
+if (doesdrive && age > 18) {
     console.log("You can drive");
 }
-else{
+else {
     console.log("You cannot drive");
 }
 
-for(let i = 0;i <= 5;i++){
+for (let i = 0; i <= 5; i++) {
     console.log(i);
 }
+
+let j = 0;
+while (j <= 10) {
+    console.log(j);
+    j += 1;
+} 
