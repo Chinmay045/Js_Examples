0cd76f9ace36d919be9bc9a76b743f8fd8af6c44
Chinmay <chinz.sandur07@gmail.com>
2023-11-03 11:48:24 +0530

    add example of function

---
+++ 08/index.js
@@ -0,0 +1,7 @@
+console.log("this is 8 th lesson");
+function greet(name){
+    console.log(`Happy birthday ${name}! You are only young once so enjoy it to the fullest. Time passes way too fast and you don’t get these years back.`);
+}
+
+greet("Chinmay");
+
