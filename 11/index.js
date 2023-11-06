9e6cac0e9783e714a1b5e857e027c07afc30926c
Chinmay <chinz.sandur07@gmail.com>
2023-11-06 11:24:31 +0530

    add JS exercise

---
+++ 11/index.js
@@ -0,0 +1,8 @@
+let b = document.links;
+            let a = "javscript";
+            let c = Array.from(b);
+            c.forEach(function(element) {
+                if (element.href.includes(a)) {
+                    console.log(element);
+                }
+            })
