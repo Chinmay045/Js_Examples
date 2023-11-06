a53070a3ef0e2006eaf30ea43f9472ed2bbda8c4
Chinmay <chinz.sandur07@gmail.com>
2023-11-06 11:46:32 +0530

    add example for innerHtml

--- 12/index.js
+++ 12/index.js
@@ -3,5 +3,6 @@ console.log("Welcome to tutorial 13");
 let element = document.getElementById("first");
  element.parentNode;
  element.style.color = 'red';
-element.innerText = "Chinmay is a good boy"
+element.innerText = "Chinmay is a good boy";
+element.innerHTML = '<b>Harry is a good Boy </b>'
 console.log(element);
