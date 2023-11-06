11edc2cfa295a0ffb1a7984ebf06b7131ea2d4d2
Chinmay <chinz.sandur07@gmail.com>
2023-11-06 12:04:46 +0530

    add example for getElementByClassName

--- 12/index.js
+++ 12/index.js
@@ -1,8 +1,17 @@
 console.log("Welcome to tutorial 13");
 
 let element = document.getElementById("first");
- element.parentNode;
- element.style.color = 'red';
-element.innerText = "Chinmay is a good boy";
+// element.parentNode;
+// element.style.color = 'red';
+// element.innerText = "Chinmay is a good boy";
 element.innerHTML = '<b>Harry is a good Boy </b>'
-console.log(element);
+// console.log(element);
+
+let sel = document.querySelector('#first');
+// sel = document.querySelector(.chile);
+ sel = document.querySelector('div');
+
+console.log(sel);
+
+let ele = document.getElementsByClassName('chile');
+console.log(ele[3]);
