3482d8374e03a6bb09a3beb24205b7d685a4cc40
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 10:02:16 +0530

    add example on appending ul to this class

--- 14/index.js
+++ 14/index.js
@@ -1,7 +1,12 @@
 console.log("Hello");
 
 let element = document.createElement('li');
-element.className = "childUl";
+element.className = "childUll";
 element.id = 'createdLi';
 element.setAttribute('title', 'mytitle')
+element.textContent = 'hello'
 console.log(element);
+
+let ul = document.querySelector('ul.this');
+ul.appendChild(element);
+console.log(ul);
