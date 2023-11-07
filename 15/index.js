39e271116a99d326f64b3ca78c65796393a3a75e
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 11:33:24 +0530

    add example on dbclick

--- 15/index.js
+++ 15/index.js
@@ -1,10 +1,21 @@
 
-document.getElementById('heading').addEventListener
-    ('click', function (e) {
-        let variable;
-        console.log("You have clicked the heading");
-        // location.href = '//codewithharry.com'
-        variable = e.target;
-        variable = e.target.id;
-        console.log(variable);
-    })
+// document.getElementById('heading').addEventListener
+//     ('click', function (e) {
+//         let variable;
+//         console.log("You have clicked the heading");
+//         // location.href = '//codewithharry.com'
+//         variable = e.target;
+//         variable = e.target.id;
+//         console.log(variable);
+//         variable = Array.from(e.target.classList);
+//         console.log(variable);
+//     })
+
+let btn = document.getElementById('btn');
+btn.addEventListener('dblclick', func1);
+
+function func1(e) {
+    console.log("Thanks",e);
+    e.preventDefault();
+}
+console.log(btn);
