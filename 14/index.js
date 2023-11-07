ab69cc74fc4da0b09bb6d90570de60765f0787da
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 10:23:34 +0530

    add example of replace

--- 14/index.js
+++ 14/index.js
@@ -4,9 +4,23 @@ let element = document.createElement('li');
 element.className = "childUll";
 element.id = 'createdLi';
 element.setAttribute('title', 'mytitle')
-element.textContent = 'hello'
+element.innerHTML = '<p><b>hello</b></p>'
 console.log(element);
+console.log(element);
+
+// let text = document.createTextNode("I am a text node");
+// element.appendChild(text);
 
 let ul = document.querySelector('ul.this');
 ul.appendChild(element);
 console.log(ul);
+
+let elem2 = document.createElement('h3');
+elem2.id = 'elem2';
+elem2.className = 'elem2';
+let tnode = document.createTextNode("this is Node2");
+elem2.appendChild(tnode);
+console.log(elem2);
+// console.log(tnode);
+element.replaceWith(elem2);
+
