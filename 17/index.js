6fe1f5599d29a75ce7a4e35b5e9b55676954f373
Chinmay <chinz.sandur07@gmail.com>
2023-11-08 11:31:13 +0530

    add example for local storage clear()

--- 17/index.js
+++ 17/index.js
@@ -1,4 +1,9 @@
+
+//Add a key-value pair inside local storage
 localStorage.setItem('Name', "Chinmay");
+
+//Clears the entire the local storage
+// localStorage.clear();
 
 let name = localStorage.getItem("Name");
 console.log(name);
