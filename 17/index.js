8f3ea75f83820f6c823d4808b7965d60bda10d50
Chinmay <chinz.sandur07@gmail.com>
2023-11-08 13:11:31 +0530

    add HTML structure using Bootstrap

--- 17/index.js
+++ 17/index.js
@@ -1,9 +1,13 @@
-
+let Array = ['adrak', 'pyaz', 'Bhindi'];
 //Add a key-value pair inside local storage
 localStorage.setItem('Name', "Chinmay");
-
+localStorage.setItem('Name2', 'Rohan');
+localStorage.setItem('sabzi', JSON.stringify(Array));
 //Clears the entire the local storage
 // localStorage.clear();
 
+
+//Retrieve an element
 let name = localStorage.getItem("Name");
+name = JSON.parse(localStorage.getItem('sabzi'));
 console.log(name);
