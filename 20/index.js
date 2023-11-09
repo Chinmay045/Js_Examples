f7d8f73ab385e51e6dfc80ae2138e3cb19375813
Chinmay <chinz.sandur07@gmail.com>
2023-11-09 10:58:36 +0530

    add example of Date

--- 20/index.js
+++ 20/index.js
@@ -2,6 +2,11 @@ let today = new Date();
 console.log(today);
 let otherDate = new Date('08-04-2023  04:54:08');
 otherDate = new Date('June 13 1976');
+otherDate.setDate(23);
+otherDate.setHours(2);
+otherDate.setMinutes(22);
+otherDate.setSeconds(45);
 console.log(otherDate);
 let a = otherDate.getDay();
+
 console.log(a);
