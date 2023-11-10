985368dca0c1f0e8dcc85cc7bd850704211d5d5e
Chinmay <chinz.sandur07@gmail.com>
2023-11-10 11:04:28 +0530

    add example for Objects literal

--- 22/index.js
+++ 22/index.js
@@ -2,11 +2,26 @@
 let car = {
     name: 'Maruti 800',
     topSpeed: 89,
-    run: function(){
+    run: function () {
         console.log("car is running");
     }
 }
 
 console.log(car);
 
- 
+function GeneralCar(givenName, givenSpeed) {
+    this.name = givenName;
+    this.topSpeed = givenSpeed;
+    this.run = function () {
+        console.log(`${this.name} is running`);
+    }
+    this.analyze = function () {
+        console.log(`This car is slower by ${200 - this.topSpeed} than Mercedes`);
+    }
+}
+
+car1 = new GeneralCar('Nisan', 180);
+car2 = new GeneralCar("Maruti Alto", 80);
+console.log(car1);
+let c1 = car2.run();
+console.log(c1);
