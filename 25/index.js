9b076870aa2210bc48961d7813a85a2ef7dec591
Chinmay <chinz.sandur07@gmail.com>
2023-11-16 12:09:57 +0530

    add if/else condition with checked

--- 25/index.js
+++ 25/index.js
@@ -11,6 +11,26 @@ function Book(name, author, type) {
 let libraryForm = document.getElementById('libraryForm')
 libraryForm.addEventListener('submit', libraryFormSubmit);
 
-function libraryFormSubmit() {
+function libraryFormSubmit(e) {
     console.log("You have submited Library Form!!");
+    let name = document.getElementById('bookname').value;
+    let author = document.getElementById('authorname').value;
+    // let type = document.getElementById('type').value;
+    let type;
+    let fiction = document.getElementById('fiction')
+    let programming = document.getElementById('programming')
+    let cooking = document.getElementById('cooking')
+
+
+    if (fiction && fiction.checked) {
+        type = fiction.value;
+    } else if (programming && programming.checked) {
+        type = programming.value;
+    } else if (cooking && cooking.checked) {
+        type = cooking.value;
+    }
+    let book = new Book(name, author, type)
+    console.log(book);
+    e.preventDefault();
+
 }
