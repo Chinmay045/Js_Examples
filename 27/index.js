c3113020e4af02fd0022e5e52af148fcbb733402
Chinmay <chinz.sandur07@gmail.com>
2023-11-16 16:11:50 +0530

    add list of result in html page using template literal

--- 27/index.js
+++ 27/index.js
@@ -29,3 +29,42 @@ function buttonClickHandler() {
 
     console.log("We are done");
 }
+
+let popBtn = document.getElementById('backUp');
+popBtn.addEventListener('click', popHandler)
+
+function popHandler() {
+    console.log("you have clicked the popHandler");
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://dummy.restapiexample.com/api/v1/employees", true)
+
+    xhr.onprogress = function () {
+        console.log('On progress');
+    }
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            let list = document.getElementById('list')
+            str = "";
+            for (key in obj) {
+                str += `<li> ${obj[key]}</li>`
+            }
+            list.innerHTML = str;
+
+        }
+        else {
+            console.error("Some error ooccured");
+        }
+    }
+
+    xhr.onreadystatechange = function () {
+        console.log("Ready state is ", xhr.readyState);
+    }
+
+    xhr.send();
+
+    console.log("We are done");
+}
