b763175d8f523312498733918213742822850311
Chinmay <chinz.sandur07@gmail.com>
2023-11-16 12:44:14 +0530

    add js logic to table

--- 25/index.js
+++ 25/index.js
@@ -6,6 +6,26 @@ function Book(name, author, type) {
     this.type = type;
 }
 
+//Display constructor
+function Display() {
+
+}
+
+Display.prototype.add = function (book) {
+    console.log("Adding to UI");
+    let tableBody = document.getElementById('tableBody');
+    let uiString = `   <tr>
+    <td>${book.name}</td>
+    <td>${book.author}</td>
+    <td>${book.type}</td> 
+</tr>`
+    tableBody.innerHTML = uiString;
+}
+Display.prototype.clear = function () {
+    let libraryForm = document.getElementById('libraryForm')
+    libraryForm.reset();
+}
+
 //Add submit event listener to libraryForm
 
 let libraryForm = document.getElementById('libraryForm')
@@ -17,9 +37,9 @@ function libraryFormSubmit(e) {
     let author = document.getElementById('authorname').value;
     // let type = document.getElementById('type').value;
     let type;
-    let fiction = document.getElementById('fiction')
+    let fiction = document.getElementById('author')
     let programming = document.getElementById('programming')
-    let cooking = document.getElementById('cooking')
+    let cooking = document.getElementById('Cooking')
 
 
     if (fiction && fiction.checked) {
@@ -31,6 +51,10 @@ function libraryFormSubmit(e) {
     }
     let book = new Book(name, author, type)
     console.log(book);
+
+    let display = new Display();
+    display.add(book);
+    display.clear();
     e.preventDefault();
 
 }
