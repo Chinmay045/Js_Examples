fadab9cd7b2e837256d335c3b55ca8073ffd927f
Chinmay <chinz.sandur07@gmail.com>
2023-11-16 15:42:37 +0530

    add js example for onreadystatechange

--- 27/index.js
+++ 27/index.js
@@ -7,13 +7,23 @@ function buttonClickHandler() {
     console.log("you have clicked the FetchButton");
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", 'harry.txt', true);
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
 
-    xhr.onprogress = function(){
+    xhr.onprogress = function () {
         console.log('On progress');
     }
 
-    xhr.onload = function() {
-        console.log(this.responseText);
+    xhr.onload = function () {
+        if (this.status === 200) {
+            console.log(this.responseText);
+        }
+        else {
+            console.error("Some error ooccured");
+        }
     }
+
+    xhr.onreadystatechange = function () {
+        console.log("Ready state is ", xhr.readyState);
+    }
+    xhr.send();
 }
