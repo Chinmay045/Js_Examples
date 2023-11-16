2b09c56aeb47ee5b11d58f8c5c74e51833880c26
Chinmay <chinz.sandur07@gmail.com>
2023-11-16 10:01:43 +0530

    add example for inheritance

--- 24/index.js
+++ 24/index.js
@@ -9,11 +9,38 @@ class Employee {
     slogan() {
         return ` I am ${this.name} and this company is the best!!`
     }
+    joiningYear() {
+        return 2020 - this.experience;
+    }
 
     static add(a, b) {
         return a + b;
     }
+
 }
 
 exam = new Employee("Chinmay", 4, "Division");
 console.log(exam);
+
+class Programmer extends Employee {
+    constructor(givenName, givenExperience, givenDivision, Language, github) {
+        super(givenName, givenExperience, givenDivision);
+        this.Language = Language;
+        this.github = github;
+    }
+
+    favouriteLanguage() {
+        if (this.language == 'Python') {
+            return "python"
+        }
+        else {
+            return "Javascript";
+        }
+    }
+
+    static multiply(a, b) {
+        return a * b;
+    }
+}
+rohan = new Programmer("Rohan", "3", "Lays", "Go", "Rohan420")
+console.log(rohan);
