694abd24b38b462352a443b608dd2942c9af3cd2
Chinmay <chinz.sandur07@gmail.com>
2023-11-16 11:33:25 +0530

    add eventListener to button

--- 25/index.js
+++ 25/index.js
@@ -1,2 +1,16 @@
 console.log("Helloo");
 
+function Book(name, author, type) {
+    this.name = name;
+    this.author = author;
+    this.type = type;
+}
+
+//Add submit event listener to libraryForm
+
+let libraryForm = document.getElementById('libraryForm')
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit() {
+    console.log("You have submited Library Form!!");
+}
