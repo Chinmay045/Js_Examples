3859b95799288dd40d15d59e9511092e213bc810
Chinmay <chinz.sandur07@gmail.com>
2023-11-17 12:09:17 +0530

    execute using callback function

--- 29/index.js
+++ 29/index.js
@@ -1,25 +1,27 @@
 const students = [
-    {name : "harry", subject:"Javascript"},
-    {name :"Rohan", subject:"Machine Learning"}
+    { name: "harry", subject: "Javascript" },
+    { name: "Rohan", subject: "Machine Learning" }
 ]
 
-function enrollStudent(student){
-    setTimeout(function(){
+function enrollStudent(student, callback) {
+    setTimeout(function () {
         students.push(student)
-console.log("success");
-    },3000)
+        console.log("success");
+        callback();
+    }, 3000)
 }
 
-function getStudents(){
-    setTimeout(function(){
+function getStudents() {
+    setTimeout(function () {
         let str = "";
-        students.forEach(function(student){
+        students.forEach(function (student) {
             str += `<li> ${student.name} </li>`
         });
         document.getElementById('students').innerHTML = str;
-    },1000);
+        console.log("Results fetched");
+    }, 1000);
 }
 
-let newStudent = {name:"Sunny", Subject: "Python"}
-enrollStudent(newStudent);
-getStudents();
+let newStudent = { name: "Sunny", Subject: "Python" }
+enrollStudent(newStudent, getStudents());
+// getStudents();
