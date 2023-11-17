361944086575f9ae31da993e790433c9864cb8fd
Chinmay <chinz.sandur07@gmail.com>
2023-11-17 11:43:01 +0530

    add example for functions and class

---
+++ 28/index.js
@@ -0,0 +1,20 @@
+class library{
+    constructor(booklist){
+        this.booklist = booklist;
+        this.issuedBook = {};
+    }
+
+    getBookList(){
+        this.booklist.forEach(element => {
+            console.log(element);
+        });
+    }
+
+    issueBook(bookname, user){
+        this.issuedBook[bookname] = user;
+    }
+
+    returnBook(bookname){
+        delete this.issuedBook[bookname];
+    }
+}
