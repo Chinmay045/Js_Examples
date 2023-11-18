35cb80622d9c8e645a4848facc2d450d82bfb777
Chinmay <chinz.sandur07@gmail.com>
2023-11-18 11:07:04 +0530

    add eample for fetch api with json response in JS

--- 32/index.js
+++ 32/index.js
@@ -2,12 +2,17 @@ let myBtn = document.getElementById("myBtn");
 let content = document.getElementById("content");
 
 function getData() {
-    url = "harry.txt";
+    console.log("Started Getdata")
+    url = "https://api.github.com/users";
     fetch(url).then((response) => {
-        return response.text();
+        console.log("Inside First then")
+        return response.json();
     }).then((data) => {
+        console.log("Inside Second then")
         console.log(data);
     })
 }
 
+console.log("Before running getdata");
 getData();
+console.log("After running getdata");
