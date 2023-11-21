9dfe1fdac18cb6b9375e36315e9f2bde5acc384b
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 11:58:02 +0530

    add example for regular expressions with case insensitive

--- 37/index.js
+++ 37/index.js
@@ -1,10 +1,12 @@
 let reg = /Chinz/g;
+reg = /Chinz/i; //case insensitivity
+
 console.log(reg);
 console.log(reg.source)
 
-let s = "This is great code Chinz with Chinz";
+let s = "This is great KLcode CHinz with Chinz";
 
 let result = reg.exec(s)
 console.log(result)
- result = reg.exec(s)
-console.log(result)
+// result = reg.exec(s)
+// console.log(result)
