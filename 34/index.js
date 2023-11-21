6a130b8a935c8ed62a0524028c58b2619f6da2b8
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 10:50:57 +0530

    add example for try/catch

--- 34/index.js
+++ 34/index.js
@@ -1,14 +1,16 @@
 console.log("Hello!!")
 
 async function harry() {
+    console.log("Inside Harry Function");
     const response = await fetch("https://api.github.com/users");
-    // console.log(response);
     console.log("Beffore Response");
     const users = await response.json();
-    // console.log(users)
+    console.log("users resolved");
     return users;
 }
 
+console.log("Beffore calling Harry");
 let fun = harry();
+console.log("After calling Harry");
 fun.then(data => console.log(data))
 console.log(fun);
---
+++ 35/index.js
@@ -0,0 +1,10 @@
+console.log("Hello")
+
+try {
+    console.log("WE are Inside try  block");
+
+    functionAdina()
+}
+catch (err) {
+    console.log(err);
+}
