a63e662a991799a0ea632b8c346500a057046298
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 11:53:08 +0530

    add example for regular expressions with global flag

--- 37/index.js
+++ 37/index.js
@@ -1,8 +1,10 @@
-let reg = /Chinz/;
+let reg = /Chinz/g;
 console.log(reg);
 console.log(reg.source)
 
-let s = "This is great code with Chinz";
+let s = "This is great code Chinz with Chinz";
 
 let result = reg.exec(s)
+console.log(result)
+ result = reg.exec(s)
 console.log(result)
