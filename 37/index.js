55ab7c39438cd1a4b7fd043d757cd1d30b38bdaa
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 12:13:57 +0530

    add example for match in regular expression

--- 37/index.js
+++ 37/index.js
@@ -1,12 +1,19 @@
 let reg = /Chinz/g;
-reg = /Chinz/i; //case insensitivity
+reg = /Chinz/; //case insensitivity
 
+let re = /gog/
 console.log(reg);
 console.log(reg.source)
 
-let s = "This is great KLcode CHinz with Chinz";
+let s = "This gog is great KLcode Chinz with CChinz";
 
-let result = reg.exec(s)
-console.log(result)
+// let result = reg.exec(s)
+// console.log(result.index)
 // result = reg.exec(s)
 // console.log(result)
+
+let result = reg.test(s);
+console.log(result);
+
+let res = s.match(re);
+console.log(res);
