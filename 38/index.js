f296abdae1c6a8f3b094f2d02e96e7a7fb99eccb
Chinmay <chinz.sandur07@gmail.com>
2023-11-22 15:55:56 +0530

    add example for regex with test

--- 38/index.js
+++ 38/index.js
@@ -1,6 +1,10 @@
-let regex = /chinmay/;
+let regex = /chiny/;
 
 let str = "chinmay is agood boy, chinmay code is chinmay";
 
 let result = regex.exec(str);
 console.log(result)
+
+if (regex.test(str)) {
+    console.log(`The string ${str} matches the expression ${regex.source}`);
+}
