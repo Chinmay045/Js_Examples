b7c33a7f0c2cc5a02746a91fbc5327dd62ed7633
Chinmay <chinz.sandur07@gmail.com>
2023-11-22 21:29:41 +0530

    add array example with const

--- 39/index.js
+++ 39/index.js
@@ -1,2 +1,11 @@
 const cars = ["Saab", "Volvo", "BMW"];
+
+// You can create a constant array:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// You can change an element:
+cars[0] = "Toyota";
+
+// You can add an element:
+cars.push("Audi");
 console.log(cars)
