966bff96b993aa89caec11b3bb3a3d5fee9ff629
Chinmay <chinz.sandur07@gmail.com>
2023-11-22 21:28:59 +0530

    add array example with const

--- 38/index.js
+++ 38/index.js
@@ -2,6 +2,7 @@ let regex = /^c/;
 regex = /y$/;
 regex = /ch.nmay/;
 regex = /ch*inmay/;
+regex = /chi?nmayi?/;
 let str = "chinmay is agood boy, chinmay code is chinmay";
 
 let result = regex.exec(str);
