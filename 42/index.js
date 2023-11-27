58d372abeba93985ee4b4177c138e3dba6cedd38
Chinmay <chinz.sandur07@gmail.com>
2023-11-27 11:55:53 +0530

    add regex

--- 42/index.js
+++ 42/index.js
@@ -3,16 +3,23 @@ const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 
 // console.log(name, email, phone);
-name.addEventListener("blur",()=> {
+name.addEventListener("blur", () => {
     console.log("Name is blurred");
+    let regex = /^[a-zA-z]([0-9a-zA-Z]){0,10}/;
+    let str = name.value;
+    console.log(regex, str);
+    if (regex.test(str)) {
+        console.log("it is matched!!")
+    }
 })
 
 // console.log(name, email, phone);
-email.addEventListener("blur",()=> {
+email.addEventListener("blur", () => {
     console.log("email is blurred");
 })
 
 // console.log(name, email, phone);
-phone.addEventListener("blur",()=> {
+phone.addEventListener("blur", () => {
     console.log("phone is blurred");
 })
+
