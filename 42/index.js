83ad16501fd1864cdff26bf4812c32976622688c
Chinmay <chinz.sandur07@gmail.com>
2023-11-27 12:22:56 +0530

    add regex to phone

--- 42/index.js
+++ 42/index.js
@@ -5,23 +5,48 @@ const phone = document.getElementById('phone');
 // console.log(name, email, phone);
 name.addEventListener("blur", () => {
     console.log("Name is blurred");
-    let regex = /^[a-zA-z]([0-9a-zA-Z]){0,10}/;
+    let regex = /^[a-zA-z]([0-9a-zA-Z]){1,10}/;
     let str = name.value;
     console.log(regex, str);
     if (regex.test(str)) {
         console.log("it is matched!!")
+        name.classList.remove("is-invalid");
+
     }else{
         console.log("no match");
+        name.classList.add("is-invalid");
     }
 })
 
 // console.log(name, email, phone);
 email.addEventListener("blur", () => {
     console.log("email is blurred");
+    let regex = /^[a-zA-z]([0-9a-zA-Z]){1,10}$/;
+    let str = email.value;
+    console.log(regex, str);
+    if (regex.test(str)) {
+        console.log("it is matched!!")
+        email.classList.remove("is-invalid");
+
+    }else{
+        console.log("no match");
+        email.classList.add("is-invalid");
+    }
 })
 
 // console.log(name, email, phone);
 phone.addEventListener("blur", () => {
     console.log("phone is blurred");
+    let regex = /^([0-9]){10}$/;
+    let str = phone.value;
+    console.log(regex, str);
+    if (regex.test(str)) {
+        console.log("it is matched!!")
+        phone.classList.remove("is-invalid");
+
+    }else{
+        console.log("no match");
+        phone.classList.add("is-invalid");
+    }
 })
 
