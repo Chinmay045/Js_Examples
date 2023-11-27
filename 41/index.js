a097a03b9bf9acc0952ed86e129b4361990084ab
Chinmay <chinz.sandur07@gmail.com>
2023-11-27 10:36:24 +0530

    add example of regex character classes

---
+++ 41/index.js
@@ -0,0 +1,13 @@
+
+let regex = /\wart/
+str = "Harry bhai"
+
+
+let result = regex.exec(str);
+console.log("The result from exec is ", result);
+
+if (regex.test(str)) {
+    console.log(`The string ${str} matches the expression ${regex.source}`);
+} else {
+    console.log(`the ${str} string does not match the expression`);
+}
