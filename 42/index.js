d6f2f117cad67bfc7b4b856cb4b835e871456f49
Chinmay <chinz.sandur07@gmail.com>
2023-11-27 13:07:36 +0530

    finished project

--- 42/index.js
+++ 42/index.js
@@ -1,62 +1,99 @@
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
+let validEmail = false;
+let validPhone = false;
+let validUser = false;
+$('#failure').hide();
+$('#success').hide();
 
 // console.log(name, email, phone);
-name.addEventListener("blur", () => {
-    console.log("Name is blurred");
-    let regex = /^[a-zA-z]([0-9a-zA-Z]){1,10}/;
+name.addEventListener('blur', () => {
+    console.log("name is blurred");
+    // Validate name here
+    let regex = /^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
     let str = name.value;
     console.log(regex, str);
     if (regex.test(str)) {
-        console.log("it is matched!!")
-        name.classList.remove("is-invalid");
+        console.log('Your name is valid');
+        name.classList.remove('is-invalid');
+        validUser = true;
+    }
+    else {
+        console.log('Your name is not valid');
+        name.classList.add('is-invalid');
+        validUser = false;
 
-    } else {
-        console.log("no match");
-        name.classList.add("is-invalid");
     }
 })
 
-// console.log(name, email, phone);
-email.addEventListener("blur", () => {
+email.addEventListener('blur', () => {
     console.log("email is blurred");
+    // Validate email here
     let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
     let str = email.value;
     console.log(regex, str);
     if (regex.test(str)) {
-        console.log("it is matched!!")
-        email.classList.remove("is-invalid");
-
-    } else {
-        console.log("no match");
-        email.classList.add("is-invalid");
+        console.log('Your email is valid');
+        email.classList.remove('is-invalid');
+        validEmail = true;
+    }
+    else {
+        console.log('Your email is not valid');
+        email.classList.add('is-invalid');
+        validEmail = false;
     }
 })
 
-// console.log(name, email, phone);
-phone.addEventListener("blur", () => {
+phone.addEventListener('blur', () => {
     console.log("phone is blurred");
+    // Validate phone here
     let regex = /^([0-9]){10}$/;
     let str = phone.value;
     console.log(regex, str);
     if (regex.test(str)) {
-        console.log("your phone is valid")
-        phone.classList.remove("is-invalid");
+        console.log('Your phone is valid');
+        phone.classList.remove('is-invalid');
+        validPhone = true;
+    }
+    else {
+        console.log('Your phone is not valid');
+        phone.classList.add('is-invalid');
+        validPhone = false;
 
-    } else {
-        console.log("no match");
-        phone.classList.add("is-invalid");
     }
 })
 
-let submit = document.getElementById("submit");
-submit.addEventListener("click", (e) => {
-   
-        e.preventDefault();
-        console.log("You clicked on submit");
-        
-    
-    let success = document.getElementById("success");
-    success.classList.add("show");
+let submit = document.getElementById('submit');
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log('You clicked on submit');
+    // console.log(validEmail, validUser, validPhone);
+
+    // Submit your form here
+    if (validEmail && validUser && validPhone) {
+        let failure = document.getElementById('failure');
+
+        console.log('Phone, email and user are valid. Submitting the form');
+        let success = document.getElementById('success');
+        success.classList.add('show');
+        // failure.classList.remove('show');
+        // $('#failure').alert('close');
+        $('#failure').hide();
+        $('#success').show();
+
+    }
+    else {
+        console.log('One of Phone, email or user are not valid. Hence not submitting the form. Please correct the errors and try again');
+        let failure = document.getElementById('failure');
+        failure.classList.add('show');
+        // success.classList.remove('show');
+        // $('#success').alert('hide');
+        $('#success').hide();
+        $('#failure').show();
+    }
+
+
+
 })
